2e3ad956b3481c722f3c6802bb5cd660a346687d
Web_M <shiao.mei@vanmilk.com>
2022-10-08 16:52:36 +0800

    日常维护

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import App from "./App";
 import "./index.less";
 import "./locales/index";
 //React18搭配antdlayout 会出现横向滚动条，导致屏幕闪烁，所以改用React17的渲染方式
+
+//React18 with antdlayout will have a horizontal scroll bar, which will cause the screen to flicker, so the rendering method of React17 is used instead.
 // ReactDOM.createRoot(document.getElementById("root")!).render(
 //   <Provider store={store}>
 //     <App />
