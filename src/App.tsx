59a8941ceae6cc73a3d821ed9795d519862bc28e
Web_M <shiao.mei@vanmilk.com>
2022-11-02 14:19:40 +0800

    优化代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { HashRouter } from "react-router-dom";
 import Router from "./routers/index";
 import enUS from "antd/es/locale/en_US";
 import zhCN from "antd/es/locale/zh_CN";
-import "./app.less";
 import { ConfigProvider } from "antd";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "./redux";
 import AuthRouter from "@/routers/auth";
+
+import "./app.less";
+
 function App() {
   const [i18nLocale, setI18nLocale] = useState(zhCN);
   const lange = useSelector((state: RootState) => state.language.lange);
   useEffect(() => {
-    if (lange == "en") return setI18nLocale(enUS);
-    if (lange == "zhCn") return setI18nLocale(zhCN);
+    if (lange == "en") setI18nLocale(enUS);
+    if (lange == "zhCn") setI18nLocale(zhCN);
   }, [lange]);
   return (
     <div className="App">
