7e003d1f4cc3b959eaedd69c91af27288c20c23e
Web_M <design_duke@163.com>
2023-11-10 17:22:14 +0800

    动态渲染菜单

--- src/layout/Menu/index.tsx
+++ src/layout/Menu/index.tsx
@@ -1,13 +1,13 @@
 import Items from "./item";
 import { Menu } from "antd";
-import type { MenuProps } from "antd";
 import * as Icons from "@ant-design/icons";
 import { getOpenKeys } from "@/utils/utils";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
 export default () => {
   const { pathname } = useLocation();
+  const [menu, setMenu] = useState<any>([]);
   const [selectedKeys, setSelectedKeys] = useState<string[]>([pathname]);
   const [openKeys, setOpenKeys] = useState<string[]>([]);
   const onOpenChange = (openKeys: string[]) => {
@@ -17,46 +17,34 @@ export default () => {
     if (latestOpenKey.includes(openKeys[0])) return setOpenKeys(openKeys);
     setOpenKeys([latestOpenKey]);
   };
-  type MenuItem = Required<MenuProps>["items"][number];
-  const getItem = (
-    label: React.ReactNode,
-    key?: React.Key | null,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-    type?: "group"
-  ): MenuItem => {
-    return {
-      key,
-      icon,
-      children,
-      label,
-      type,
-    } as MenuItem;
-  };
+
   // 动态渲染 Icon 图标
   const customIcons: { [key: string]: any } = Icons;
   const addIcon = (name: string) => React.createElement(customIcons[name]);
 
   // 处理后台返回菜单 key 值为 antd 菜单需要的 key 值
-  const deepLoopFloat = (menuList: any, newArr: any = []) => {
-    menuList.forEach((item: any) => {
-      // 下面判断代码解释 *** !item?.children?.length   ==>   (!item.children || item.children.length === 0)
-      if (!item?.children?.length)
-        return newArr.push(getItem(item.title, item.path, addIcon(item.icon!)));
-      newArr.push(
-        getItem(
-          item.title,
-          item.path,
-          addIcon(item.icon!),
-          deepLoopFloat(item.children)
-        )
-      );
+  const convertToMenu2 = (menuData: any[]) => {
+    return menuData.map((item) => {
+      const { key, icon, label, path, children } = item;
+
+      const iconElement = addIcon(icon);
+      const labelElement = children ? label : <Link to={path}>{label}</Link>;
+
+      const menuItem = {
+        key,
+        icon: iconElement,
+        label: labelElement,
+        children: null,
+      };
+
+      if (children) {
+        //@ts-ignore
+        menuItem.children = convertToMenu2(children);
+      }
+
+      return menuItem;
     });
-    return newArr;
   };
-  /**
-   * 后期可以把菜单的数据从后端调接口获取
-   */
 
   // 刷新页面菜单保持高亮
   useEffect(() => {
@@ -65,13 +53,18 @@ export default () => {
 
   useEffect(() => {
     setOpenKeys(getOpenKeys(pathname));
+    // 后期可以把菜单的数据从后端调接口获取
+    setTimeout(() => {
+      const item = convertToMenu2(Items);
+      setMenu(item);
+    }, 1000);
   }, []);
 
   return (
     <Menu
       theme="dark"
       mode="inline"
-      items={Items}
+      items={menu}
       selectedKeys={selectedKeys}
       openKeys={openKeys}
       onOpenChange={onOpenChange}
--- src/layout/Menu/item.tsx
+++ src/layout/Menu/item.tsx
@@ -1,51 +1,48 @@
-import {
-  AppstoreOutlined,
-  BugOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
-import { Link } from "react-router-dom";
-
 const menu = [
   {
     key: "/Home",
-    icon: <VideoCameraOutlined />,
-    label: <Link to={"/Home"}>Home</Link>,
+    icon: "VideoCameraOutlined",
+    label: "Home",
+    path: "/Home",
   },
   {
     key: "/subOne",
-    icon: <UploadOutlined />,
+    icon: "UploadOutlined",
     label: "Menu",
     children: [
       {
         key: "/subOne/count",
-        icon: <UserOutlined />,
-        label: <Link to="/subOne/count">Count</Link>,
+        icon: "UserOutlined",
+        label: "Count",
+        path: "/subOne/count",
       },
       {
         key: "/subOne/table",
-        icon: <UploadOutlined />,
-        label: <Link to="/subOne/table">Table</Link>,
+        icon: "UploadOutlined",
+        label: "Table",
+        path: "/subOne/table",
       },
       {
         key: "/subOne/communication",
-        icon: <UploadOutlined />,
-        label: <Link to="/subOne/communication">Communication</Link>,
+        icon: "UploadOutlined",
+        label: "Communication",
+        path: "/subOne/communication",
       },
     ],
   },
   {
     key: "/subTwo",
-    icon: <AppstoreOutlined />,
+    icon: "AppstoreOutlined",
     label: "Link",
     children: [
       {
         key: "/subTwo/link",
-        icon: <BugOutlined />,
-        label: <Link to="/subTwo/link">GitHub地址</Link>,
+        icon: "BugOutlined",
+        label: "GitHub地址",
+        path: "/subTwo/link",
       },
     ],
   },
 ];
+
 export default menu;
