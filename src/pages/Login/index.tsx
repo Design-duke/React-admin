654cbb614f9687f7fa98b5163820dfbec7d23d97
梅世奥 <gcw_DXKB7j1o@noreply.gitcode.com>
2025-11-12 14:00:26 +0800

    1

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -21,7 +21,11 @@ function Login() {
         name="normal_login"
         className="min-w-[328px] max-w-[500px] h-[60vh]"
         style={{ margin: "0 auto" }}
-        initialValues={{ remember: true }}
+        initialValues={{
+          username: "admin", 
+          password: "12345",
+          remember: true,
+        }}
         onFinish={onFinish}
       >
         <Form.Item
