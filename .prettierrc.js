da05f32a9d9615a935bdaedd13eda8ee96ec7c58
梅世奥 <gcw_DXKB7j1o@noreply.gitcode.com>
2025-12-08 09:41:28 +0800

    feat(tailwindcss): tailwindcss类自动排序

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,14 @@
 export default {
-  semi: true,                  // 语句结尾加分号
-  singleQuote: false,           // 使用单引号
-  jsxSingleQuote: false,       // JSX 中使用双引号（React 社区习惯）
-  trailingComma: 'es5',        // 对象数组末尾加逗号
-  tabWidth: 2,                 // 缩进空格数
-  useTabs: false,              // 不使用 tab
-  printWidth: 80,              // 换行长度
-  endOfLine: 'lf',             // 换行符：\n (Unix)
-  arrowParens: 'always',       // 箭头函数单参数也加括号
-  proseWrap: 'preserve',       // Markdown 换行
-  embeddedLanguageFormatting: 'auto', // 是否格式化模板字符串中的 HTML/CSS
+  semi: true, // 语句结尾加分号
+  singleQuote: false, // 使用单引号
+  jsxSingleQuote: false, // JSX 中使用双引号（React 社区习惯）
+  trailingComma: "es5", // 对象数组末尾加逗号
+  tabWidth: 2, // 缩进空格数
+  useTabs: false, // 不使用 tab
+  printWidth: 80, // 换行长度
+  endOfLine: "lf", // 换行符：\n (Unix)
+  arrowParens: "always", // 箭头函数单参数也加括号
+  proseWrap: "preserve", // Markdown 换行
+  embeddedLanguageFormatting: "auto", // 是否格式化模板字符串中的 HTML/CSS
+  plugins: ["prettier-plugin-tailwindcss"],
 };
